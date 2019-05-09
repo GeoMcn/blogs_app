2c9d07d82854da1238d99d3d347ada50927ff00b
George McNally <geomcn11@gmail.com>
2019-05-09 14:13:10 +0100

    added sign out tests

---
+++ spec/features/signing_users_out_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.feature "Signing out signed in users" do
+    before do
+        @john = User.create!(email: "john@gmail.com", password: "examplepassword")
+        
+        visit "/"
+        
+        click_link "Sign in"
+        
+        fill_in "Email", with: "john@gmail.com" 
+        fill_in "Password", with: "examplepassword"
+        
+        click_button "Log in"
+    end
+    
+    scenario "sign out successfully" do
+        visit "/"
+        
+        click_link "Sign out"
+        
+        expect(page).to have_content("Signed out successfully")
+        expect(page).not_to have_link("Sign out")
+        expect(page).to have_link("Sign in")
+        expect(page).to have_link("Sign up")
+        
+    end
+    
+end
