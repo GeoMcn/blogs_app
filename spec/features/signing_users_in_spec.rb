c8c9ba4a71b1bd8aabeb62c93289502914ba713d
George McNally <geomcn11@gmail.com>
2019-05-09 13:54:44 +0100

    set up user sign in

---
+++ spec/features/signing_users_in_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+RSpec.feature "Sign in users" do
+    before do
+        @john = User.create!(email: "john@gmail.com", password: "examplepassword")
+    end
+    
+    scenario "With valid credentials" do
+        visit "/"
+        
+        click_link "Sign in"
+        
+        fill_in "Email", with: "john@gmail.com" 
+        fill_in "Password", with: "examplepassword"
+        
+        click_button "Log in"
+        
+        expect(page).to have_content("Signed in successfully")
+        expect(page).to have_content("Signed in as #{@john.email}")
+        expect(page).not_to have_link("Sign in")
+        expect(page).not_to have_link("Sign up")
+        
+    end
+    
+    scenario "With invalid credentials" do
+        visit "/"
+        
+        click_link "Sign in"
+        
+        fill_in "Email", with: "" 
+        fill_in "Password", with: ""
+        
+        click_button "Log in"
+        
+        expect(page).to have_content("Invalid Email or password.")
+        
+    end
+end
